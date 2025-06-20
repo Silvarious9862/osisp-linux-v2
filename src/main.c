fe843635271073423c9f3dad9bdb5d9b18a028cf
Silvarious9862 <vbobrik0@gmail.com>
2025-06-20 15:24:04 +0300

    удаление потоков

--- src/main.c
+++ src/main.c
@@ -53,6 +53,8 @@ int main() {
            "  - : добавить поток-потребитель\n"
            "  > : увеличить размер очереди\n"
            "  < : уменьшить размер очереди (только если очередь пуста)\n"
+           "  d : удалить производителя\n"
+           "  f : удалить потребителя\n"
            "  p : вывести состояние очереди\n"
            "  q : завершение работы\n");
 
@@ -67,7 +69,7 @@ int main() {
         } else if (command[0] == '+') {
             int *id = malloc(sizeof(int));
             if (!id) continue;
-            *id = ++prod_count;
+            *id = ++prod_count;  // присваиваем новый номер производителя
             if (pthread_create(&producers[prod_count - 1], NULL, producer_thread, id) != 0) {
                 perror("Ошибка создания производителя");
                 free(id);
@@ -75,10 +77,40 @@ int main() {
         } else if (command[0] == '-') {
             int *id = malloc(sizeof(int));
             if (!id) continue;
-            *id = ++cons_count;
+            *id = ++cons_count;  // присваиваем новый номер потребителя
             if (pthread_create(&consumers[cons_count - 1], NULL, consumer_thread, id) != 0) {
                 perror("Ошибка создания потребителя");
                 free(id);
+            }
+        }
+        // Новая команда: удаление производителя в порядке LIFO
+        else if (command[0] == 'd') {
+            if (prod_count > 0) {
+                int removed_number = prod_count;
+                if (pthread_cancel(producers[prod_count - 1]) == 0) {
+                    pthread_join(producers[prod_count - 1], NULL);
+                    prod_count--;
+                    printf("Удалён производитель [%d]\n", removed_number);
+                } else {
+                    printf("Ошибка при удалении производителя.\n");
+                }
+            } else {
+                printf("Нет активных производителей для удаления.\n");
+            }
+        }
+        // Новая команда: удаление потребителя в порядке LIFO
+        else if (command[0] == 'f') {
+            if (cons_count > 0) {
+                int removed_number = cons_count;
+                if (pthread_cancel(consumers[cons_count - 1]) == 0) {
+                    pthread_join(consumers[cons_count - 1], NULL);
+                    cons_count--;
+                    printf("Удалён потребитель [%d]\n", removed_number);
+                } else {
+                    printf("Ошибка при удалении потребителя.\n");
+                }
+            } else {
+                printf("Нет активных потребителей для удаления.\n");
             }
         } else if (command[0] == 'p') {
             print_status(&queue);
@@ -119,7 +151,7 @@ int main() {
     pthread_cond_broadcast(&queue.not_empty);
     pthread_cond_broadcast(&queue.not_full);
     pthread_mutex_unlock(&queue.mutex);
-    
+
     for (int i = 0; i < prod_count; i++) {
         pthread_join(producers[i], NULL);
     }
