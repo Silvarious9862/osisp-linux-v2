3bf35700357936d3bd89a1b4fe20ce1e4e0ab5c4
Silvarious9862 <vbobrik0@gmail.com>
2025-06-20 18:37:36 +0300

    тесты с valgrind

--- test/test_release.sh
+++ test/test_release.sh
@@ -3,22 +3,27 @@
 
 # 1. Генерация файла с заданным числом записей.
 echo "Генерация файла с 512000 записями..."
-build/release/gen 512000 test/testfile
-
+valgrind build/release/gen 512000 test/testfile
+echo "--------------------------------------------------------------------------------"
 # 2. Первичный просмотр файла.
 #    Выводим только первые 10 строк и последние 10 строк.
-echo "Просмотр файла до сортировки (первая часть):"
-build/release/view test/testfile | head -n 10
-echo "Просмотр файла до сортировки (последняя часть):"
-build/release/view test/testfile | tail -n 10
-
+echo "Просмотр файла до сортировки:"
+valgrind build/release/view ./test/testfile > tmp
+echo "Начало:"
+head -n 10 tmp
+echo "Конец:"
+tail -n 10 tmp
+echo "--------------------------------------------------------------------------------"
 # 3. Сортировка файла.
 #    Параметры: memsize = 1048576 байт (1 МБ), granul = 4 (2^4=16 блоков), threads = 8.
 echo "Сортировка файла..."
-build/release/sort_index 1048576 4 8 test/testfile
-
+valgrind build/release/sort_index 1048576 4 8 test/testfile
+echo "--------------------------------------------------------------------------------"
 # 4. Просмотр файла после сортировки.
-echo "Просмотр файла после сортировки (первая часть):"
-build/release/view test/testfile | head -n 10
-echo "Просмотр файла после сортировки (последняя часть):"
-build/release/view test/testfile | tail -n 10
+echo "Просмотр файла после сортировки:"
+valgrind build/release/view ./test/testfile > tmp
+echo "Начало:"
+head -n 10 tmp
+echo "Конец:"
+tail -n 10 tmp
+rm tmp
